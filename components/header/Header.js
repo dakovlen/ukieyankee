cd6c9b073fcdda2222b862cee2ed414430d9b2bc
Dakovlen <dakovlen@gmail.com>
2021-07-20 11:47:25 +0300

    Add tailwindcss to project

--- components/header/Header.js
+++ components/header/Header.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function Header({ navigation }) {
     return(
       <header className="header">
-        <div className="container header-content">
+        <div className="container header-content ">
           <Link href="/">
               <a className="logo-link">LOGO</a>
             {/*<a><img src="https://res.cloudinary.com/detgwqdyd/image/upload/v1626712796/logo_bjoc78.png" alt="logo"/></a>*/}
